50f7f5d020ce1caf9c398bbe416da2c06a37a6c2
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-12 23:42:02 +0300

    Stock API'ın paymenta ve ordera yayınlayacağı eventleri ve consumer işlemleri tamamlandı.

--- Shared/RabbitMQSettings.cs
+++ Shared/RabbitMQSettings.cs
@@ -8,6 +8,8 @@ namespace Shared
 {
     public static class RabbitMQSettings
     {
+        //isimlendirirken dinleyecek olan api ve neye karşılık dinlecek event
         public const string Stock_OrderCreatedEventQueue = "stock-order-created-event-queue";
+        public const string Payment_StockReservedEventQueue = "payment-stock-reserved-event-queue";
     }
 }
--- Stock.API/Consumers/OrderCreatedEventConsumer.cs
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -1,6 +1,68 @@
-﻿namespace Stock.API.Consumers
+﻿using MassTransit;
+using MongoDB.Driver;
+using Shared;
+using Shared.Events;
+using Stock.API.Services;
+
+namespace Stock.API.Consumers
 {
-    public class OrderCreatedEventConsumer
+    public class OrderCreatedEventConsumer(MongoDBService mongoDBService, ISendEndpointProvider sendEndpointProvider, IPublishEndpoint publishEndpoint) : IConsumer<OrderCreatedEvent>
     {
+        //readonly MongoDBService _mongoDBService;
+
+        // public OrderCreatedEventConsumer(MongoDBService mongoDBService)
+        // {
+        //     _mongoDBService = mongoDBService;
+        // }
+
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            List<bool> stockResult = new();
+            var collection = mongoDBService.GetCollection<Models.Stock>();
+
+            foreach (var orderItem in context.Message.OrderItems)
+            {
+                stockResult.Add(await (await collection.FindAsync(s => s.ProductId == orderItem.ProductId && s.Count >= orderItem.Count)).AnyAsync());
+            }
+            if (stockResult.TrueForAll(s => s.Equals(true)))
+            {
+                //stock güncellemesi..
+                foreach (var orderItem in context.Message.OrderItems)
+                {
+                    Models.Stock stock = await (await collection.FindAsync(s => s.ProductId == orderItem.ProductId)).FirstOrDefaultAsync();
+
+                    stock.Count -= orderItem.Count;
+                    await collection.FindOneAndReplaceAsync(x => x.ProductId == orderItem.ProductId, stock);
+                }
+                //Payment'i uyaracak eventin fırlatırması.
+
+                var sendEndPoint = await sendEndpointProvider.GetSendEndpoint(new Uri($"queue:{RabbitMQSettings.Payment_StockReservedEventQueue}"));
+                //yayınlayacağımız eventin instance'ı
+                StockReservedEvent stockReservedEvent = new StockReservedEvent()
+                {
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    TotalPrice = context.Message.TotalPrice,
+                    OrderItems = context.Message.OrderItems,
+                };
+
+                await sendEndPoint.Send(stockReservedEvent);
+            }
+            else
+            {
+                //Stock işlemi başarısız...
+                //Orderı uyaracak event fırlatılacaktır..
+                StockNotReservedEvent stockNotReservedEvent = new StockNotReservedEvent()
+                {
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    Message = "Stok miktarı yetersiz..."
+                };
+
+                await publishEndpoint.Publish(stockNotReservedEvent);
+            }
+        }
+
+
     }
 }
